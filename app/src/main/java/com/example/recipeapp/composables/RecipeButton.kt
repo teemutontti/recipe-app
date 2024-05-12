ef918f319274b5d2a10d45abd1a0d873ca0c7733
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-13 01:52:14 +0300

    Add: Separate recipe button to its own file.

---
+++ app/src/main/java/com/example/recipeapp/composables/RecipeButton.kt
@@ -0,0 +1,86 @@
+package com.example.recipeapp.composables
+
+import android.transition.CircularPropagation
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.recipeapp.Recipe
+
+@Composable
+fun RecipeButton(recipe: Recipe) {
+    var loading by remember { mutableStateOf(true) }
+    var imageError by remember { mutableStateOf(false) }
+
+    Button(
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.onBackground
+        ),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.secondary),
+        modifier = Modifier.padding(16.dp),
+        onClick = { /*TODO*/ }
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Box(contentAlignment = Alignment.Center, modifier = Modifier
+                .padding(end = 10.dp, top = 10.dp, bottom = 10.dp)
+                .clip(RoundedCornerShape(15.dp))
+                .border(8.dp, MaterialTheme.colorScheme.surfaceVariant, RoundedCornerShape(15.dp))
+            ) {
+                if (loading && !imageError) CircularProgressIndicator()
+                if (imageError) Text("Image Error")
+                AsyncImage(
+                    model = recipe.image,
+                    contentDescription = "${recipe.label} picture",
+                    onSuccess = { (_) -> loading = false },
+                    onError = { (_) -> imageError = true },
+                    modifier = Modifier
+                        .height(125.dp)
+                        .width(125.dp)
+                )
+            }
+            Text(
+                text = recipe.label,
+                style = TextStyle(
+                    fontSize = 20.sp
+                )
+            )
+        }
+    }
+}

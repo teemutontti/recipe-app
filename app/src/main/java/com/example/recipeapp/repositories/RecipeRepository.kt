495a19bbbbfcf63b20b2ef211012e481089980c5
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-15 23:43:01 +0300

    Add: SearcRecipes function.

--- app/src/main/java/com/example/recipeapp/repositories/RecipeRepository.kt
+++ app/src/main/java/com/example/recipeapp/repositories/RecipeRepository.kt
@@ -10,8 +10,10 @@ import com.example.recipeapp.api.Instructions
 import com.example.recipeapp.services.RetrofitInstance
 import com.example.recipeapp.utils.SharedPreferencesManager
 import com.example.recipeapp.api.Recipe
+import com.example.recipeapp.api.SearchResponse
 import com.example.recipeapp.api.SpoonacularResponse
 import retrofit2.Response
+import retrofit2.http.Query
 
 object RecipeRepository: ViewModel() {
     private var _specials: SnapshotStateList<Recipe> = mutableStateListOf<Recipe>()
@@ -19,6 +21,9 @@ object RecipeRepository: ViewModel() {
 
     private var _selectedRecipe: Recipe? = null
     val selectedRecipe: Recipe? get() = _selectedRecipe
+
+    private var _searchResults: SnapshotStateList<Recipe> = mutableStateListOf<Recipe>()
+    val searchResults: List<Recipe> get() = _searchResults
 
     private suspend fun fetchRandomMeal(context: Context, mealType: String): Recipe? {
         val response: Response<SpoonacularResponse> = RetrofitInstance().recipeService
@@ -67,5 +72,23 @@ object RecipeRepository: ViewModel() {
 
     fun updateSelectedRecipe(recipe: Recipe) {
         _selectedRecipe = recipe
+    }
+
+    suspend fun searchRecipes(context: Context, query: String) {
+        try {
+            _searchResults.clear()
+            val response: Response<SearchResponse> = RetrofitInstance().recipeService.searchRecipes(
+                apiKey = API_KEY,
+                query = query,
+                number = 10
+            )
+            if (response.isSuccessful) {
+                response.body()?.results?.map {
+                    _searchResults.add(it)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }

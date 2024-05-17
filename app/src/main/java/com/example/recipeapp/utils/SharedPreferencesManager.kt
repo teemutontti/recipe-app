2162ea44a00ae7b2e403f54a660f4c6e16facb26
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-17 13:14:08 +0300

    Add: Save favourites and own recipes in shared prefs.

--- app/src/main/java/com/example/recipeapp/utils/SharedPreferencesManager.kt
+++ app/src/main/java/com/example/recipeapp/utils/SharedPreferencesManager.kt
@@ -11,6 +11,8 @@ object SharedPreferencesManager {
     val TODAYS_SPECIAL_MEAL_TYPES = listOf("breakfast", "lunch", "dinner")
     private const val PREFS_NAME = "recipe_prefs"
     private const val TODAYS_SPECIALS_LAST_LOAD_KEY = "todays_specials_load_date"
+    private const val FAVOURITES_KEY = "favourite_recipes"
+    private const val OWN_RECIPES_KEY = "own_recipes"
 
     fun isTodaysSpecialsLoaded(context: Context): Boolean {
         val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
@@ -45,5 +47,40 @@ object SharedPreferencesManager {
         Log.d("SharedPrefs", specials.toString())
 
         return specials
+    }
+
+    fun getFavourites(context: Context): List<Recipe> {
+        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val jsonFavourites = prefs.getString(FAVOURITES_KEY, null) ?: return listOf()
+        val favourites = Gson().fromJson(jsonFavourites, Array<Recipe>::class.java).toList()
+
+        Log.d("SharedPrefs", "Favourites: $favourites")
+        return favourites
+    }
+
+    fun updateFavourites(context: Context, newFavourites: List<Recipe>) {
+        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val prefEditor: SharedPreferences.Editor = prefs.edit()
+
+        prefEditor.putString(FAVOURITES_KEY, Gson().toJson(newFavourites))
+
+        prefEditor.commit()
+    }
+
+    fun getOwnRecipes(context: Context): List<Recipe> {
+        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val json = prefs.getString(OWN_RECIPES_KEY, null) ?: return listOf()
+        val ownRecipes = Gson().fromJson(json, Array<Recipe>::class.java).toList()
+
+        Log.d("SharedPrefs", "Own recipes: $ownRecipes")
+        return ownRecipes
+    }
+
+    fun updateOwnRecipe(context: Context, newOwnRecipe: List<Recipe>) {
+        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val prefEditor: SharedPreferences.Editor = prefs.edit()
+
+        prefEditor.putString(OWN_RECIPES_KEY, Gson().toJson(newOwnRecipe))
+        prefEditor.commit()
     }
 }

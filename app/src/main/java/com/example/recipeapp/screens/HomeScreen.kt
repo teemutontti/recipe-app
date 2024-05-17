42caa8d225456024b404a0afa58fc40f04b2823e
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-17 13:47:04 +0300

    Change: Display specials in a scrollable row.

--- app/src/main/java/com/example/recipeapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/recipeapp/screens/HomeScreen.kt
@@ -2,50 +2,42 @@ package com.example.recipeapp.screens
 
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.recipeapp.composables.NavBar
 import com.example.recipeapp.composables.RecipeButton
-import com.example.recipeapp.composables.RecipeList
-import com.example.recipeapp.composables.TopBar
 import com.example.recipeapp.repositories.RecipeRepository
-import kotlinx.coroutines.delay
 
 @Composable
 fun HomeScreen(navController: NavController) {
     Scaffold(
-        topBar = { TopBar(title = "Recipe App") },
         content = {
             HomeScreenContent(navController = navController, paddingValues = it)
         },
@@ -78,12 +70,33 @@ private fun HomeScreenContent(navController: NavController, paddingValues: Paddi
             else {
                 Text("Today's Specials:", style = TextStyle(fontSize = 24.sp))
                 Spacer(modifier = Modifier.height(16.dp))
-                RecipeList(
-                    navController = navController,
-                    recipes = recipeViewModel.specials,
-                    onEmptyMessage = "Couldn't find specials",
-                    showAdditionalRecipeInfo = true,
-                )
+                LazyRow(modifier = Modifier.fillMaxWidth()) {
+                    itemsIndexed(recipeViewModel.specials) { index, item ->
+                        Column(modifier = Modifier.width(296.dp)) {
+                            Text(
+                                text = when (index) {
+                                    0 -> "Breakfast"
+                                    1 -> "Lunch"
+                                    2 -> "Dinner"
+                                    else -> "Snack"
+                                },
+                                style = TextStyle(
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            RecipeButton(
+                                navController = navController,
+                                recipe = item,
+                                showAdditionalInfo = true
+                            )
+                        }
+                        if (index < recipeViewModel.specials.size - 1) {
+                            Spacer(modifier = Modifier.width(24.dp))
+                        }
+                    }
+                }
                 TextButton(onClick = { navController.navigate("add_recipe") }) {
                     Text("Add New Recipe")
                 }

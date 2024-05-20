4b6d0130f656e9e2595d716ec9326e331b15b0ca
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-21 00:55:04 +0300

    Change: Use CachedRecipes and save favourites as a list.

--- app/src/main/java/com/example/recipeapp/utils/SharedPreferencesManager.kt
+++ app/src/main/java/com/example/recipeapp/utils/SharedPreferencesManager.kt
@@ -3,14 +3,15 @@ package com.example.recipeapp.utils
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
+import com.example.recipeapp.api.CachedRecipe
 import com.example.recipeapp.api.Recipe
 import com.google.gson.Gson
 import java.time.LocalDate
 
 object SharedPreferencesManager {
-    val TODAYS_SPECIAL_MEAL_TYPES = listOf("breakfast", "lunch", "dinner")
     private const val PREFS_NAME = "recipe_prefs"
     private const val TODAYS_SPECIALS_LAST_LOAD_KEY = "todays_specials_load_date"
+    private const val TODAYS_SPECIALS_KEY = "todays_specials"
     private const val FAVOURITES_KEY = "favourite_recipes"
     private const val OWN_RECIPES_KEY = "own_recipes"
 
@@ -23,42 +24,41 @@ object SharedPreferencesManager {
         return prefs.getString(TODAYS_SPECIALS_LAST_LOAD_KEY, null) == currentDate
     }
 
-    fun saveTodaysSpecial(context: Context, recipe: Recipe, mealType: String) {
+    fun saveTodaysSpecials(context: Context, recipes: List<CachedRecipe?>) {
         Log.d("RecipeRepository", "Saving recipe to shared prefs")
 
         val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         val prefEditor: SharedPreferences.Editor = prefs.edit()
 
-        prefEditor.putString("todays_${mealType}_special", Gson().toJson(recipe))
+        prefEditor.putString(TODAYS_SPECIALS_KEY, Gson().toJson(recipes))
         prefEditor.putString(TODAYS_SPECIALS_LAST_LOAD_KEY, LocalDate.now().toString())
 
         prefEditor.commit()
     }
 
-    fun getTodaysSpecials(context: Context): List<Recipe> {
+    fun getTodaysSpecials(context: Context): List<CachedRecipe> {
         Log.d("RecipeRepository", "Getting todays specials from shared prefs")
 
         val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
 
-        val specials: List<Recipe> = TODAYS_SPECIAL_MEAL_TYPES.map {
-            val recipeJson: String = prefs.getString("todays_${it}_special", null) ?: return listOf()
-            Gson().fromJson(recipeJson, Recipe::class.java)
-        }
+        val json: String = prefs.getString(TODAYS_SPECIALS_KEY, null) ?: return listOf()
+        val specials = Gson().fromJson(json, Array<CachedRecipe>::class.java).toList()
+
         Log.d("SharedPrefs", specials.toString())
 
         return specials
     }
 
-    fun getFavourites(context: Context): List<Recipe> {
+    fun getFavourites(context: Context): List<CachedRecipe> {
         val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         val jsonFavourites = prefs.getString(FAVOURITES_KEY, null) ?: return listOf()
-        val favourites = Gson().fromJson(jsonFavourites, Array<Recipe>::class.java).toList()
+        val favourites = Gson().fromJson(jsonFavourites, Array<CachedRecipe>::class.java).toList()
 
         Log.d("SharedPrefs", "Favourites: $favourites")
         return favourites
     }
 
-    fun updateFavourites(context: Context, newFavourites: List<Recipe>) {
+    fun updateFavourites(context: Context, newFavourites: List<CachedRecipe>) {
         val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         val prefEditor: SharedPreferences.Editor = prefs.edit()
 

e9c7bc8107b01cd6e22a1c59412ecdef21e1fc4d
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-22 01:44:26 +0300

    Change: Form progress navigation in bottom bar.

--- app/src/main/java/com/example/recipeapp/screens/AddRecipeScreen.kt
+++ app/src/main/java/com/example/recipeapp/screens/AddRecipeScreen.kt
@@ -1,23 +1,41 @@
 package com.example.recipeapp.screens
 
+import android.util.Log
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Save
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -26,14 +44,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -48,92 +71,38 @@ import com.example.recipeapp.api.Measures
 import com.example.recipeapp.api.Recipe
 import com.example.recipeapp.api.SingleMeasure
 import com.example.recipeapp.api.Step
+import com.example.recipeapp.composables.IngredientForm
 import com.example.recipeapp.composables.NumberCounter
 import com.example.recipeapp.composables.StepIndicator
 import com.example.recipeapp.composables.TopBar
 import com.example.recipeapp.repositories.RecipeRepository
+import com.example.recipeapp.utils.AddableIngredient
+import com.example.recipeapp.utils.Utils
 
 @Composable
 fun AddRecipeScreen(navController: NavController) {
-    Scaffold(
-        content = {
-            AddRecipeScreenContent(navController = navController, paddingValues = it)
-        },
-    )
-}
-
-@Composable
-private fun AddRecipeScreenContent(navController: NavController, paddingValues: PaddingValues) {
-    val context = LocalContext.current
-    val recipeViewModel: RecipeRepository = viewModel(LocalContext.current as ComponentActivity)
-
-    val emptyIngredient = Ingredient(
-        name = "",
-        measures = Measures(
-            metric = SingleMeasure(
-                amount = 1.0,
-                unitShort = ""
-            ),
-            us = SingleMeasure(
-                amount = 1.0,
-                unitShort = "",
-            )
-        ),
-    )
-    val emptyInstructionStep = Step(
-        number = 1,
-        step = ""
-    )
-
-    var title: String by remember { mutableStateOf("") }
-    // TODO: Add image picker option
-    //var image: String by remember { mutableStateOf("") }
-    var servings: Int by remember { mutableIntStateOf(2) }
-    val ingredients = remember { mutableStateListOf(emptyIngredient) }
-    val instructions = remember { mutableStateListOf(emptyInstructionStep) }
     var currentFormStep by remember { mutableIntStateOf(0) }
-
-    fun onRecipeSave() {
-        // TODO: Add data validation
-        val newRecipe = Recipe(
-            id = 123456789,
-            title = title,
-            image = "",
-            servings = servings,
-            readyInMinutes = 0,
-            license = "",
-            sourceName = "",
-            sourceUrl = "",
-            extendedIngredients = ingredients,
-            instructions = instructions.joinToString(". "),
-            analyzedInstructions = listOf(Instructions(name = "", steps = instructions))
-        )
-        recipeViewModel.addOwnRecipe(context, newRecipe)
-    }
+    var allowNext by remember { mutableStateOf(false) }
 
     fun handleStepChange(direction: Int) {
         val newCurrentFormStep = currentFormStep + direction
         if (newCurrentFormStep in 0..3) {
             currentFormStep = newCurrentFormStep
         }
-    }
-
-    fun handleBackClick() {
-        if (currentFormStep == 0) {
-            navController.navigateUp()
+        if (direction < 0) {
+            allowNext = true
         } else {
-            handleStepChange(-1)
+            allowNext = false
         }
     }
 
+    fun handleAllowNextChange(newValue: Boolean) {
+        allowNext = newValue
+    }
 
-
-    Column(modifier = Modifier
-        .padding(paddingValues)
-        .verticalScroll(rememberScrollState())
-    ) {
-        Column(modifier = Modifier.padding(24.dp)) {
-            TextButton(onClick = ::handleBackClick) {
+    Scaffold(
+        topBar = {
+            TextButton(onClick = { navController.navigateUp() }) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Icon(
                         imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
@@ -146,128 +115,48 @@ private fun AddRecipeScreenContent(navController: NavController, paddingValues: 
                     Text("Back")
                 }
             }
-            StepIndicator(
-                steps = 0..3,
-                currentStep = currentFormStep,
+        },
+        content = {
+            AddRecipeScreenContent(
+                navController = navController,
+                paddingValues = it,
+                currentFormStep = currentFormStep,
+                handleAllowNextChange = ::handleAllowNextChange
             )
-            when (currentFormStep) {
-                0 -> TitleStep(::handleStepChange)
-                1 -> IngredientsStep(::handleStepChange)
-                2 -> InstructionsStep(::handleStepChange)
-                3 -> PreviewStep(::handleStepChange)
-            }
-
-            /*
-            TextField(
-                value = title,
-                onValueChange = { title = it },
-                placeholder = { Text("Insert title...") },
-                label = { Text("Title *") },
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            NumberCounter(
-                defaultValue = servings,
-                onNumberChange = { servings = it },
-                prefix = "Serving size:",
-                max = 20
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = "Ingredients",
-                style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            ingredients.forEachIndexed { index, s ->
-                Column(
-                    modifier = Modifier
-                        .background(MaterialTheme.colorScheme.surface)
-                        .padding(16.dp)
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier.fillMaxWidth()
+        },
+        bottomBar = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+            ) {
+                if (currentFormStep > 0) {
+                    Button(
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(44.dp),
+                        onClick = { handleStepChange(-1) },
+                        border = BorderStroke(2.dp, MaterialTheme.colorScheme.surface),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = MaterialTheme.colorScheme.onSurface
+                        ),
                     ) {
-                        Text("Ingredient #${index+1}")
-                        if (index > 0) IconButton(onClick = { ingredients.removeAt(index) }) {
-                            Icon(Icons.Rounded.Clear, "Remove ingredient icon")
-                        }
+                        Text("Previous")
                     }
-                    Spacer(modifier = Modifier.height(8.dp))
-                    TextField(
-                        value = s.name,
-                        onValueChange = { ingredients[index] = ingredients[index].copy(name = it) },
-                        label = { Text("Name") },
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Row {
-                        TextField(
-                            value = "${s.measures.metric.amount}",
-                            onValueChange = { ingredients[index] = ingredients[index].copy(
-                                measures = Measures(
-                                    metric = SingleMeasure(
-                                        amount = it.toFloat(),
-                                        unitShort = ingredients[index].measures.metric.unitShort
-                                    ),
-                                    us = ingredients[index].measures.us
-                                )
-                            )},
-                            label = { Text("Amount") },
-                            modifier = Modifier
-                                .weight(0.5f)
-                                .padding(0.dp)
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        TextField(
-                            value = s.measures.metric.unitShort,
-                            onValueChange = { ingredients[index] = ingredients[index].copy(
-                                measures = Measures(
-                                    metric = SingleMeasure(
-                                        amount = ingredients[index].measures.metric.amount,
-                                        unitShort = it
-                                    ),
-                                    us = ingredients[index].measures.us
-                                )
-                            ) },
-                            label = { Text("Unit") },
-                            modifier = Modifier.weight(0.5f)
-                        )
-                    }
+                    Spacer(modifier = Modifier.width(16.dp))
                 }
-                Spacer(modifier = Modifier.height(8.dp))
-            }
-            TextButton(onClick = { ingredients.add(emptyIngredient) }) {
-                Icon(imageVector = Icons.Rounded.Add, contentDescription = "Add icon")
-                Text("Add Ingredient")
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = "Instructions",
-                style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            instructions.forEachIndexed { index, s ->
-                TextField(
-                    value = s.step,
-                    onValueChange = { instructions[index] = instructions[index].copy(
-                        number = index + 1,
-                        step = it
-                    ) },
-                    label = { Text("Step ${index+1}") },
-                    modifier = Modifier.fillMaxWidth(),
-                    trailingIcon = {
-                        if (index > 0) IconButton(onClick = { instructions.removeAt(index) }) {
-                            Icon(Icons.Rounded.Clear, "Remove ingredient icon")
-                        }
-                        else null
-                    }
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-            }
-            TextButton(onClick = { instructions.add(emptyInstructionStep) }) {
-                Icon(imageVector = Icons.Rounded.Add, contentDescription = "Add icon")
-                Text("Add Instruction")
+                Button(
+                    enabled = allowNext,
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(44.dp),
+                    onClick = { handleStepChange(1) }
+                ) {
+                    Text("Next")
+                }
             }
             Spacer(modifier = Modifier.height(16.dp))
             Button(

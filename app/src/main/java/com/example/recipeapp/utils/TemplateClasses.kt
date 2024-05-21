e6bf5985cc344645047b1513aee6bd764639d4b7
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-22 01:40:18 +0300

    Add: File for data classes.

--- app/src/main/java/com/example/recipeapp/utils/TemplateClasses.kt
+++ app/src/main/java/com/example/recipeapp/utils/TemplateClasses.kt
@@ -1,2 +1,13 @@
 package com.example.recipeapp.utils
 
+data class CachedRecipe(
+    val id: Number,
+    val image: String,
+    val title: String,
+)
+
+data class AddableIngredient(
+    val name: String,
+    val amount: Int,
+    val unit: String
+)

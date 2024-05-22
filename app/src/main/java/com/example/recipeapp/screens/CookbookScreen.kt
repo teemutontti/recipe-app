3359719656175a13810a4576dc94031ae5f0fa7f
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-22 06:31:58 +0300

    Change: Fetch on empty lists and use add recipe button.

--- app/src/main/java/com/example/recipeapp/screens/CookbookScreen.kt
+++ app/src/main/java/com/example/recipeapp/screens/CookbookScreen.kt
@@ -1,9 +1,12 @@
 package com.example.recipeapp.screens
 
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,11 +24,13 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.recipeapp.composables.AddRecipeButton
 import com.example.recipeapp.composables.NavBar
 import com.example.recipeapp.composables.RecipeList
 import com.example.recipeapp.composables.TopBar
@@ -78,18 +83,27 @@ fun OwnRecipesTab(navController: NavController) {
     val recipeViewModel: RecipeRepository = viewModel(LocalContext.current as ComponentActivity)
 
     LaunchedEffect(Unit) {
-        recipeViewModel.fetchOwnRecipes(context)
+        if (recipeViewModel.ownRecipes.isEmpty()) {
+            recipeViewModel.fetchOwnRecipes(context)
+        }
         loading = false
     }
 
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        Button(onClick = { navController.navigate("add_recipe") }) {
-            Text("Add New Recipe")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomEnd,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            RecipeList(
+                navController = navController,
+                ownRecipes = recipeViewModel.ownRecipes
+            )
         }
-        RecipeList(
-            navController = navController,
-            ownRecipes = recipeViewModel.ownRecipes
-        )
+        AddRecipeButton(navController = navController)
     }
 }
 
@@ -100,7 +114,9 @@ fun SavedRecipesTab(navController: NavController) {
     val recipeViewModel: RecipeRepository = viewModel(LocalContext.current as ComponentActivity)
 
     LaunchedEffect(Unit) {
-        recipeViewModel.fetchFavourites(context)
+        if (recipeViewModel.favourites.isEmpty()) {
+            recipeViewModel.fetchFavourites(context)
+        }
         loading = false
     }
 

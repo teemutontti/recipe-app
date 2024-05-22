bc82881e4c673b09a51b31a1345bea4316870bad
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-22 06:25:51 +0300

    Add: File for unified add recipe button.

---
+++ app/src/main/java/com/example/recipeapp/composables/AddRecipeButton.kt
@@ -0,0 +1,58 @@
+package com.example.recipeapp.composables
+
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.recipeapp.repositories.RecipeRepository
+
+@Composable
+fun AddRecipeButton(navController: NavController) {
+    val recipeViewModel: RecipeRepository = viewModel(LocalContext.current as ComponentActivity)
+
+    Box(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        TextButton(
+            shape = CircleShape,
+            colors = ButtonDefaults.textButtonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+            ),
+            modifier = Modifier
+                .width(64.dp)
+                .height(64.dp)
+                .clip(CircleShape)
+                .shadow(2.dp),
+            onClick = {
+                recipeViewModel.setRecipeInAddition(null)
+                recipeViewModel.setIngredientInAddition(null)
+                navController.navigate("add")
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Add,
+                contentDescription = "add",
+                modifier = Modifier
+                    .width(56.dp)
+                    .height(56.dp),
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+        }
+    }
+}

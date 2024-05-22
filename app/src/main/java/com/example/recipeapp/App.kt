be49f08944acc7420f1e769951e67490d813dbb8
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-23 01:17:35 +0300

    Change: On app start get max id from own recipes.

--- app/src/main/java/com/example/recipeapp/App.kt
+++ app/src/main/java/com/example/recipeapp/App.kt
@@ -1,17 +1,32 @@
 package com.example.recipeapp
 
+import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.recipeapp.repositories.RecipeRepository
 import com.example.recipeapp.screens.RecipeEditorScreen
 import com.example.recipeapp.screens.DiscoverScreen
 import com.example.recipeapp.screens.CookbookScreen
 import com.example.recipeapp.screens.RecipeScreen
+import com.example.recipeapp.utils.Utils
 
 @Composable
 fun App() {
+    val context = LocalContext.current
     val navController = rememberNavController()
+    val recipeViewModel: RecipeRepository = viewModel(LocalContext.current as ComponentActivity)
+
+    LaunchedEffect(Unit) {
+        val maxId = recipeViewModel.getOwnRecipesMaxId(context)
+        if (maxId != null) {
+            Utils.setId(maxId)
+        }
+    }
 
     NavHost(navController = navController, startDestination = "discover") {
         composable("discover") {

2f6040ca22cf5a038a764627be82a72d0ab75935
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-24 14:31:45 +0300

    Add: Composable for user feedback message such as warnings and errors.

--- app/src/main/java/com/example/recipeapp/composables/UserFeedbackMessage.kt
+++ app/src/main/java/com/example/recipeapp/composables/UserFeedbackMessage.kt
@@ -2,12 +2,13 @@ package com.example.recipeapp.composables
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ErrorOutline
+import androidx.compose.material.icons.rounded.Warning
+import androidx.compose.material.icons.rounded.WarningAmber
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,11 +16,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ErrorMessage(message: String) {
+fun UserFeedbackMessage(message: String, type: String = "error") {
+    when (type) {
+        "error" -> Error(message)
+        "warning" -> Warning(message)
+    }
+}
+
+@Composable
+private fun Error(message: String) {
     Surface(
         shape = RoundedCornerShape(6.dp),
         color = MaterialTheme.colorScheme.error,
@@ -33,6 +41,28 @@ fun ErrorMessage(message: String) {
             Icon(
                 imageVector = Icons.Rounded.ErrorOutline,
                 contentDescription = "error",
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(message)
+        }
+    }
+}
+
+@Composable
+private fun Warning(message: String) {
+    Surface(
+        shape = RoundedCornerShape(6.dp),
+        color = MaterialTheme.colorScheme.primaryContainer,
+        contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        shadowElevation = 8.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.WarningAmber,
+                contentDescription = "warning",
             )
             Spacer(modifier = Modifier.width(8.dp))
             Text(message)

5d91b70bb7c18a7d09264f01da819a62b169473e
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-29 01:14:28 +0300

    Add: Simple remove button for unified styling.

---
+++ app/src/main/java/com/example/recipeapp/composables/RemoveButton.kt
@@ -0,0 +1,40 @@
+package com.example.recipeapp.composables
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RemoveButton(index: Int, onClick: (Int) -> Unit) {
+    IconButton(
+        onClick = { onClick(index) },
+        modifier = Modifier.height(20.dp).width(20.dp),
+        colors = IconButtonDefaults.iconButtonColors(
+            containerColor = MaterialTheme.colorScheme.errorContainer,
+            contentColor = MaterialTheme.colorScheme.onErrorContainer
+        )
+    ) {
+        Icon(
+            imageVector = Icons.Rounded.Clear,
+            contentDescription = "delete",
+            modifier = Modifier.height(16.dp).width(16.dp)
+        )
+    }
+}

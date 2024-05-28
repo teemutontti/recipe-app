eb4d8bb652d07c8a03c90da8acd4dc1c47fefb76
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-28 21:02:46 +0300

    Add: Pref keys separated to their own file.

---
+++ app/src/main/java/com/example/recipeapp/models/SharedPreferencesKeys.kt
@@ -0,0 +1,10 @@
+package com.example.recipeapp.models
+
+object SharedPreferencesKeys {
+    const val PREFS_NAME = "recipe_prefs"
+    const val PERSONAL_RECIPES = "own_recipes"
+    const val FAVOURITE_RECIPES = "favourite_recipes"
+    const val TODAYS_SPECIALS_LAST_LOAD = "todays_specials_load_date"
+    const val TODAYS_SPECIALS = "todays_specials"
+    const val RECIPE_IN_EDITING = "recipe_in_editing"
+}

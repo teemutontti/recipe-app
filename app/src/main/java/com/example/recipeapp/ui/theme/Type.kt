7d2b6e53f568714bba8813c5508744024051d2ed
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-29 19:24:42 +0300

    Add: Typography variables to MaterialTheme.

--- app/src/main/java/com/example/recipeapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/recipeapp/ui/theme/Type.kt
@@ -6,29 +6,35 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
+    //displayLarge = TextStyle(),
+    //displayMedium = TextStyle(),
+    //displaySmall = TextStyle(),
+    headlineLarge = TextStyle(
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    headlineMedium = TextStyle(
+        fontSize = 24.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+    headlineSmall = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+    //titleLarge = TextStyle(),
+    //titleMedium = TextStyle(),
+    //titleSmall = TextStyle(),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    //bodyMedium = TextStyle(),
+    //bodySmall = TextStyle(),
+    //labelLarge = TextStyle(),
+    //labelMedium = TextStyle(),
+    //labelSmall = TextStyle(),
 )

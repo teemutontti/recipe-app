e3da07ba427d6d84a58086983abe56decedcdbe5
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-30 23:34:42 +0300

    Change: Append version number and name.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.recipeapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

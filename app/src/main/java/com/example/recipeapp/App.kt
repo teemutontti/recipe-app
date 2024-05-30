c333b0bb203aeb9ab0346b42881a91ced0b2beff
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-30 08:52:30 +0300

    Change: Use ViewModelWrapper and navigate to recipe without id.

--- app/src/main/java/com/example/recipeapp/App.kt
+++ app/src/main/java/com/example/recipeapp/App.kt
@@ -1,43 +1,54 @@
 package com.example.recipeapp
 
 import android.content.Context
-import android.util.Log
-import android.widget.ViewSwitcher.ViewFactory
-import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewmodel.ViewModelFactoryDsl
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.recipeapp.repositories.RecipeRepository
 import com.example.recipeapp.screens.RecipeEditorScreen
 import com.example.recipeapp.screens.DiscoverScreen
 import com.example.recipeapp.screens.CookbookScreen
 import com.example.recipeapp.screens.RecipeScreen
-import com.example.recipeapp.utils.Utils
+import com.example.recipeapp.viewmodels.FavouriteRecipesViewModel
+import com.example.recipeapp.viewmodels.PersonalRecipesViewModel
+import com.example.recipeapp.viewmodels.RecipeUnderInspectionViewModel
+import com.example.recipeapp.viewmodels.SearchViewModel
+import com.example.recipeapp.viewmodels.TodaysSpecialsViewModel
+import com.example.recipeapp.viewmodels.ViewModelWrapper
 
 @Composable
 fun App(applicationContext: Context) {
     val navController = rememberNavController()
 
+    val inspectionViewModel: RecipeUnderInspectionViewModel = viewModel()
+    val personalRecipesViewModel: PersonalRecipesViewModel = viewModel()
+    val favouriteRecipesViewModel: FavouriteRecipesViewModel = viewModel()
+    val searchViewModel: SearchViewModel = viewModel()
+    val todaysSpecialsViewModel: TodaysSpecialsViewModel = viewModel()
+
+    val viewModels = ViewModelWrapper(
+        favourite = favouriteRecipesViewModel,
+        personal = personalRecipesViewModel,
+        inspection = inspectionViewModel,
+        search = searchViewModel,
+        specials = todaysSpecialsViewModel,
+    )
+
     CompositionLocalProvider(ApplicationContext provides applicationContext) {
         NavHost(navController = navController, startDestination = "discover") {
             composable("discover") {
-                DiscoverScreen(navController)
+                DiscoverScreen(navController, viewModels)
             }
             composable("cookbook") {
-                CookbookScreen(navController)
+                CookbookScreen(navController, viewModels)
             }
-            composable("recipe/{id}") {
-                val recipeId: Int? = it.arguments?.getString("id")?.toIntOrNull()
-                RecipeScreen(navController, recipeId)
+            composable("recipe") {
+                RecipeScreen(navController, viewModels)
             }
             composable("recipe_editor") {
-                RecipeEditorScreen(navController)
+                RecipeEditorScreen(navController, viewModels)
             }
         }
     }

6257ca3701834d34b6c0b383f415cea4e7e02e08
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-05-30 21:25:29 +0300

    Add: User feedback on rest of the views.

--- app/src/main/java/com/example/recipeapp/screens/CookbookScreen.kt
+++ app/src/main/java/com/example/recipeapp/screens/CookbookScreen.kt
@@ -4,19 +4,24 @@ import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -33,6 +38,7 @@ import com.example.recipeapp.composables.AddRecipeButton
 import com.example.recipeapp.composables.NavBar
 import com.example.recipeapp.composables.RecipeList
 import com.example.recipeapp.composables.TopBar
+import com.example.recipeapp.composables.UserFeedbackMessage
 import com.example.recipeapp.viewmodels.FavouriteRecipesViewModel
 import com.example.recipeapp.viewmodels.PersonalRecipesViewModel
 import com.example.recipeapp.viewmodels.RecipeUnderInspectionViewModel
@@ -79,30 +85,31 @@ private fun CookbookScreenContent(
 }
 
 @Composable
-private fun PersonalRecipesTab(
-    navController: NavController,
-    viewModels: ViewModelWrapper,
-) {
-    LaunchedEffect(Unit) {
-        Log.d("Cookbook", "Recipes: ${viewModels.personal.recipes.size}")
-    }
-
+private fun PersonalRecipesTab(navController: NavController, viewModels: ViewModelWrapper) {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd) {
         Column(modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())) {
-            if (viewModels.personal.loading) {
+            if (viewModels.personal.error != null) {
+                UserFeedbackMessage(viewModels.personal.error!!, "error")
+            } else if (viewModels.personal.loading) {
                 LinearProgressIndicator()
             } else {
                 RecipeList(navController, viewModels.personal.recipes, viewModels,
                     onEmpty = {
                         Column {
-                            Text(text = "You have no recipes yet...")
-                            Button(onClick = {
-                                viewModels.inspection.setRecipe(null)
-                                navController.navigate("recipe_editor")
-                            }) {
-                                Text(text = "Let's change that!")
+                            UserFeedbackMessage(message = "Seem like you have no recipes yet.")
+                            Row {
+                                Spacer(modifier = Modifier.width(16.dp))
+                                Button(
+                                    shape = RoundedCornerShape(8.dp),
+                                    onClick = {
+                                        viewModels.inspection.setRecipe(null)
+                                        navController.navigate("recipe_editor")
+                                    }
+                                ) {
+                                    Text("Let's change that!")
+                                }
                             }
                         }
                     }
@@ -116,6 +123,12 @@ private fun PersonalRecipesTab(
 @Composable
 private fun FavouriteRecipesTab(navController: NavController, viewModels: ViewModelWrapper) {
     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        RecipeList(navController, viewModels.favourite.recipes, viewModels)
+        if (viewModels.favourite.error != null) {
+            UserFeedbackMessage(viewModels.personal.error!!, "error")
+        } else if (viewModels.favourite.loading) {
+            LinearProgressIndicator()
+        } else {
+            RecipeList(navController, viewModels.favourite.recipes, viewModels)
+        }
     }
 }

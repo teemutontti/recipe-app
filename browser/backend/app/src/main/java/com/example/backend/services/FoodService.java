cbc6823721ed7ed9735934c949abe8925b8ca835
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-10-04 05:08:44 +0300

    Remove duplicate update method.

--- browser/backend/app/src/main/java/com/example/backend/services/FoodService.java
+++ browser/backend/app/src/main/java/com/example/backend/services/FoodService.java
@@ -1,7 +1,6 @@
 package com.example.backend.services;
 import com.example.backend.entities.Food;
 import com.example.backend.repositories.FoodRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,19 +13,5 @@ public class FoodService extends BaseService<Food> {
     @Override
     protected FoodRepository getRepository() {
         return foodRepository;
-    }
-
-    @Override
-    public Food update(Long id, Food food) {
-        Food existingFood = getRepository().findById(id).orElse(null);
-
-        System.out.println("\n\n" + food + "\n\n");
-        System.out.println("\n\n" + existingFood + "\n\n");
-
-        if (existingFood != null) {
-            // TODO: Implement update logic
-            return getRepository().save(existingFood);
-        }
-        return null;
     }
 }
--- browser/backend/app/src/main/java/com/example/backend/services/LogService.java
+++ browser/backend/app/src/main/java/com/example/backend/services/LogService.java
@@ -14,18 +14,4 @@ public class LogService extends BaseService<Log> {
     protected LogRepository getRepository() {
         return logRepository;
     }
-
-    @Override
-    public Log update(Long id, Log log) {
-        Log existingLog = getRepository().findById(id).orElse(null);
-
-        System.out.println("\n\n" + log + "\n\n");
-        System.out.println("\n\n" + existingLog + "\n\n");
-
-        if (existingLog != null) {
-            // TODO: Implement update logic
-            return getRepository().save(existingLog);
-        }
-        return null;
-    }
 }

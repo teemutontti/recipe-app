ef16111ca5d2e4dfb2305a0ca4e5d93b49314919
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-11-06 05:10:27 +0200

    refactor: add FailedCryptionException as a superclass for encryption and decryption exceptions

--- browser/backend/app/src/main/java/com/example/backend/exceptions/FailedDecryptionException.java
+++ browser/backend/app/src/main/java/com/example/backend/exceptions/FailedDecryptionException.java
@@ -1,6 +1,6 @@
 package com.example.backend.exceptions;
 
-public class FailedDecryptionException extends Exception {
+public class FailedDecryptionException extends FailedCryptionException {
     public FailedDecryptionException(String str) {
         super(str);
     }

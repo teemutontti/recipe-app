5900de84c503a62f1df7b4b9bb98a57f4c5afa33
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-01-15 23:58:11 +0200

    feat: add entities for food logs

--- browser/backend/app/src/main/java/com/example/backend/entities/Log.java
+++ browser/backend/app/src/main/java/com/example/backend/entities/Log.java
@@ -1,6 +1,9 @@
 package com.example.backend.entities;
 import java.sql.Date;
 import java.sql.Time;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,11 +19,14 @@ public class Log implements BaseEntity<Log> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private Date date; // NOT NULL
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate date; // NOT NULL
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
     private Time time;
     private String meal; // NOT NULL
     private Integer user;
     private Integer food;
+    private Integer amount;
 
     @Override
     public boolean update(Log log) {
@@ -30,6 +36,7 @@ public class Log implements BaseEntity<Log> {
             if (log.getMeal() != null) { setMeal(log.getMeal()); }
             if (log.getUser() != null) { setUser(log.getUser()); }
             if (log.getFood() != null) { setFood(log.getFood()); }
+            if (log.getAmount() != null) { setAmount(log.getAmount()); }
         } catch (Exception e) {
             return false;
         }

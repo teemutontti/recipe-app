91b138a36ba5f1e6c95290793526dc5876efaccc
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-01-15 17:17:41 +0200

    feat: add repositories for food logs

--- browser/backend/app/src/main/java/com/example/backend/repositories/LogRepository.java
+++ browser/backend/app/src/main/java/com/example/backend/repositories/LogRepository.java
@@ -1,5 +1,10 @@
 package com.example.backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backend.entities.Log;
+import org.springframework.http.ResponseEntity;
+import java.time.LocalDate;
+import java.util.List;
 
-public interface LogRepository extends JpaRepository<Log, Long> {}
+public interface LogRepository extends JpaRepository<Log, Long> {
+    List<Log> findByDate(LocalDate date);
+}

f762360149854f092731514e4b57217955811f60
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-01-16 00:07:03 +0200

    refactor: services to their own file and retrofit instance

--- android/app/src/main/java/com/example/recipeapp/repositories/FoodRepository.kt
+++ android/app/src/main/java/com/example/recipeapp/repositories/FoodRepository.kt
@@ -2,26 +2,6 @@ package com.example.recipeapp.repositories
 
 import com.example.recipeapp.models.Food
 import com.example.recipeapp.services.RetrofitInstance
-import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
-import retrofit2.http.Query
-
-interface FoodService {
-    @GET("api/foods")
-    suspend fun getFoods(): Response<List<Food>>
-
-    @GET("api/foods/{id}")
-    suspend fun getFoodById(@Path("id") id: Int): Response<Food>
-
-    @GET("api/foods/query")
-    suspend fun getFoodByQuery(@Query("query") query: String): Response<List<Food>>
-
-    @POST("api/foods")
-    suspend fun saveFood(@Body food: Food): Response<Food>
-}
 
 class FoodRepository {
     private val retrofitInstance = RetrofitInstance()
--- android/app/src/main/java/com/example/recipeapp/repositories/LogRepository.kt
+++ android/app/src/main/java/com/example/recipeapp/repositories/LogRepository.kt
@@ -2,32 +2,7 @@ package com.example.recipeapp.repositories
 
 import com.example.recipeapp.models.Log
 import com.example.recipeapp.services.RetrofitInstance
-import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.PATCH
-import retrofit2.http.POST
-import retrofit2.http.Path
-import retrofit2.http.Query
 import java.time.LocalDate
-
-interface LogService {
-    @GET("api/logs")
-    suspend fun getLogs(): Response<List<Log>>
-
-    @GET("api/logs/by-date")
-    suspend fun getLogsByDate(@Query("date") date: String): Response<List<Log>>
-
-    @POST("api/logs")
-    suspend fun saveLog(@Body log: Log): Response<Log>
-
-    @PATCH("api/logs/{id}")
-    suspend fun updateLog(@Path("id") id: Int, @Body log: Log): Response<Log>
-
-    @DELETE("api/logs/{id}")
-    suspend fun deleteLog(@Path("id") id: Int): Response<Void>
-}
 
 class LogRepository {
     private val retrofitInstance = RetrofitInstance()

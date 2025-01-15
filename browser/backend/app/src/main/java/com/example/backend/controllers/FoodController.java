39ca7f98507fb9e1c3446f0176d175e4b2cc163f
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-01-15 23:57:33 +0200

    feat: add controllers for food logs

--- browser/backend/app/src/main/java/com/example/backend/controllers/FoodController.java
+++ browser/backend/app/src/main/java/com/example/backend/controllers/FoodController.java
@@ -1,9 +1,21 @@
 package com.example.backend.controllers;
+import com.example.backend.entities.Log;
 import com.example.backend.services.FoodService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.backend.entities.Food;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/foods")
-public class FoodController extends BaseController<Food, FoodService> {}
+public class FoodController extends BaseController<Food, FoodService> {
+    @GetMapping("/query")
+    public ResponseEntity<List<Food>> getFoodsByQuery(@RequestParam String query) {
+        return service.getFoodsByQuery(query);
+    }
+}

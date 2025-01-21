a919fea736bca671c972c3a73950ac60a1f480ab
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-01-21 09:48:57 +0200

    fix: remove unused existsByEmail

--- browser/backend/app/src/main/java/com/example/backend/repositories/UserRepository.java
+++ browser/backend/app/src/main/java/com/example/backend/repositories/UserRepository.java
@@ -2,6 +2,4 @@ package com.example.backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backend.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Boolean existsByEmail(String email);
-}
+public interface UserRepository extends JpaRepository<User, Long> {}

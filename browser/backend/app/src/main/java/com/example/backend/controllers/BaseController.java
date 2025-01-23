011eabd2e3c0d781567fcc91c67c4172c9cd391c
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-01-23 19:07:39 +0200

    chore: rebase

--- browser/backend/app/src/main/java/com/example/backend/controllers/BaseController.java
+++ browser/backend/app/src/main/java/com/example/backend/controllers/BaseController.java
@@ -1,14 +1,15 @@
 package com.example.backend.controllers;
 import java.util.List;
+
+import com.example.backend.entities.BaseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import com.example.backend.entities.BaseEntity;
 import com.example.backend.services.BaseService;
 
 /**
@@ -16,33 +17,33 @@ import com.example.backend.services.BaseService;
  * It provides basic CRUD operations for the entity type T.
  * @param <T> Entity type to be used in the controller.
  */
-class BaseController<T extends BaseEntity<T>> {
+class BaseController<T extends BaseEntity<T>, S extends BaseService<T>> {
 
     @Autowired
-    private BaseService<T> service;
+    protected S service;
 
     @PostMapping
-    public T create(@RequestBody T entity) {
+    public ResponseEntity<T> create(@RequestBody T entity) {
         return service.create(entity);
     }
 
     @GetMapping
-    public List<T> getAll() {
+    public ResponseEntity<List<T>> getAll() {
         return service.getAll();
     }
 
     @GetMapping("/{id}")
-    public T getById(@PathVariable Long id) {
+    public ResponseEntity<T> getById(@PathVariable("id") Long id) {
         return service.getById(id);
     }
 
     @PatchMapping("/{id}")
-    public T update(@PathVariable Long id, @RequestBody T food) {
+    public ResponseEntity<T> update(@PathVariable("id") Long id, @RequestBody T food) {
         return service.update(id, food);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        service.delete(id);
+    public ResponseEntity<T> delete(@PathVariable("id") Long id) {
+        return service.delete(id);
     }
 }

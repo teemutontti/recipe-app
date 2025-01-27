59b5bb95647d1fd101b881efe388ad1fe53df4aa
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-01-27 14:24:38 +0200

    feat: allow public access to get all foods

--- browser/backend/app/src/main/java/com/example/backend/config/SecurityConfig.java
+++ browser/backend/app/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -1,19 +1,12 @@
 package com.example.backend.config;
 
-import com.example.backend.utils.SecurityUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 @Configuration
 @EnableWebSecurity
@@ -25,7 +18,6 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/api/foods").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/api/logs").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                         .requestMatchers("/api/users/login").permitAll()

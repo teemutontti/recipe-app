91d5a6f2cb0aa05307d12b0b55552aae4ec5dfd6
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-01-28 17:28:54 +0200

    feat: add formatting utils for time and float

--- android/app/src/main/java/com/example/recipeapp/utils/FormattingUtils.kt
+++ android/app/src/main/java/com/example/recipeapp/utils/FormattingUtils.kt
@@ -1,7 +1,21 @@
 package com.example.recipeapp.utils
 
+import android.util.Log
+import java.time.LocalDate
+import java.time.LocalTime
+
 object FormattingUtils {
     fun toLowerCaseCapitalizeFirst(str: String): String {
         return str[0] + str.substring(1).lowercase()
+    }
+
+    fun formatLocalTimeToString(time: LocalTime): String {
+        val split = time.toString().split(".")
+        return split[0]
+    }
+
+    fun stringToFloat(string: String): Float {
+        val convertedCommas = string.replace(",", ".")
+        return convertedCommas.toFloat()
     }
 }

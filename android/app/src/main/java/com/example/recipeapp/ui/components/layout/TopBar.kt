6423945d553ae2ad6d7a998dc939a6d6ea59d844
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-07-31 01:44:53 +0300

    refactor: topbar layout change

--- android/app/src/main/java/com/example/recipeapp/ui/components/layout/TopBar.kt
+++ android/app/src/main/java/com/example/recipeapp/ui/components/layout/TopBar.kt
@@ -1,6 +1,8 @@
 package com.example.recipeapp.ui.components.layout
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.recipeapp.ui.components.misc.UserFeedbackMessage
@@ -25,17 +28,21 @@ fun TopBar(title: String? = null, subtitle: (@Composable () -> Unit)? = null) {
     val context = LocalApplicationContext.current
     val networkConnected = checkInternetConnection(context)
 
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .padding(24.dp)) {
-        if (title != null) Text(text = title, style = MaterialTheme.typography.headlineLarge)
-        if (subtitle != null) {
-            Spacer(modifier = Modifier.height(16.dp))
-            subtitle()
-        }
-        if (!networkConnected) {
-            Spacer(modifier = Modifier.height(8.dp))
-            UserFeedbackMessage("No internet connection", type = "warning")
+    Row(
+        modifier = Modifier.padding(24.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Column {
+            if (title != null) Text(text = title, style = MaterialTheme.typography.headlineLarge)
+            if (subtitle != null) {
+                Spacer(modifier = Modifier.height(16.dp))
+                subtitle()
+            }
+            if (!networkConnected) {
+                Spacer(modifier = Modifier.height(8.dp))
+                UserFeedbackMessage("No internet connection", type = "warning")
+            }
         }
     }
 }
